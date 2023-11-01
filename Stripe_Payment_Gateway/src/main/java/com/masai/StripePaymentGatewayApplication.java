7212887a32d0b0f941511e473c244d72ea0e570a
Sonu Kumar <sonuhits047@gmail.com>
2023-11-01 12:46:55 +0530

    strpe payment gateway

---
+++ Stripe_Payment_Gateway/src/main/java/com/masai/StripePaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripePaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripePaymentGatewayApplication.class, args);
+	}
+
+}
